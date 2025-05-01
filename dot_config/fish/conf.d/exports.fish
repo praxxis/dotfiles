da1a5c9fba442afb0606cb6848c57e333ed758f6
Lindsey <praxxis@users.noreply.github.com>
2025-05-01 16:42:33 -0400

    updates

--- dot_config/fish/conf.d/exports.fish
+++ dot_config/fish/conf.d/exports.fish
@@ -1,18 +1,21 @@
 # Fish shell exports converted from bash
 
 set -gx LC_ALL "en_US.UTF-8"
-set -gx LANG "en_US"
+set -gx LANG "en_US.UTF-8"
 
 if test -d "$HOME/bin"
-    fish_add_path "$HOME/bin"
+  fish_add_path "$HOME/bin"
 end
 
 if test -d "$HOME/bin-private"
-    fish_add_path "$HOME/bin-private"
+  fish_add_path "$HOME/bin-private"
 end
 
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
 
-set -gx LESS '--quit-if-one-screen --ignore-case --status-column --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD --tabs=4 --no-init --window=-4'
+set -gx PAGER "less"
+set -gx LESS '--quit-if-one-screen --ignore-case --LONG-PROMPT --RAW-CONTROL-CHARS --tabs=2 --no-init --window=-4'
 
 set -gx HOMEBREW_CASK_OPTS "--appdir=~/Applications --fontdir=/Library/Fonts"
+
+set -gx MANPAGER "sh -c 'col -bx | bat --language man --plain --theme=\"Monokai Extended\"'"
