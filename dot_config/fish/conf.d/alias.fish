545997545cc267e4a6a4dbc720dae2a22ed78614
Lindsey <praxxis@users.noreply.github.com>
2025-05-03 18:06:07 -0400

    updates

--- dot_config/fish/conf.d/alias.fish
+++ dot_config/fish/conf.d/alias.fish
@@ -3,6 +3,7 @@ alias ls 'eza'
 alias ack 'rg --colors path:style:bold --colors line:style:bold --smart-case'
 alias fzy 'fzy -l50'
 alias cz 'chezmoi'
+alias vi 'nvim'
 
 abbr -a serve 'static-web-server --page-fallback=index.html -d .'
 abbr -a ackjs 'ack -g "*.js" -g "*.ts" -g "*.jsx" -g "*.tsx"'
