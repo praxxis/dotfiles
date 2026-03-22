d336b50e6abc0e516700df2343ffd6edc406d434
Lindsey <praxxis@users.noreply.github.com>
2026-03-22 11:28:17 -0400

    bin rearrange

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,2 +1,7 @@
 set -U fish_greeting
 fish_config theme choose "Dracula Official"
+
+# allow overriding binaries by having this first in PATH
+if test -d "$HOME/bin-private"
+  fish_add_path -m "$HOME/bin-private"
+end
